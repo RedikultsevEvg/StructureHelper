fc4f1f1db38432ffb32f2bd046370d547fb23f93
NickAppLab <nikolai-sm1996@yandex.ru>
2022-06-18 01:35:34 +0500

    Tests has been added to solution

---
+++ StructureHelperTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StructureHelperTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StructureHelperTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ac480bb-8a34-4913-b7aa-c6a5d7f35509")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StructureHelperTests/TestExampleClass.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace StructureHelperTests
+{
+    [TestClass]
+    public class TestExampleClass
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+        }
+    }
+}
